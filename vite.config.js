a8758e4551051b33b8ddceb4cb5840638be99374
Aditya-Jaiswal-082 <adityajaiswal2704@gmail.com>
2026-05-30 02:15:28 +0530

    update bug 2

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
  
 export default defineConfig({
+  base: './',
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes('node_modules/recharts') || id.includes('node_modules/d3') || id.includes('node_modules/victory')) {
+          if (id.includes('node_modules/recharts') || id.includes('node_modules/d3')) {
             return 'recharts'
           }
           if (id.includes('node_modules/react-dom') || id.includes('node_modules/react/')) {
